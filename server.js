b86e18856328701f2429a2e6045cbd53a878d2a8
TonNouveauPseudo <ton-nouvel-email@exemple.com>
2026-06-30 20:14:15 +0000

    Correction SSL pour Aiven

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session({
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false // C'est la ligne magique qui règle votre erreur
+        rejectUnauthorized: false // <--- C'est cette ligne qui va tout débloquer
     }
 });
 
