22e025768fc7f9e1ad246ba5a0b377e739863dd4
TonNouveauPseudo <ton-nouvel-email@exemple.com>
2026-09-08 22:10:49 +0000

    intallation

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'monecole-v1';
+const assetsToCache = [
+  './index.html',
+  ./login.html',
+  './accueil.html'
+  // Ajoutez vos autres fichiers essentiels ici
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
 });
 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
